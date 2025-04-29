1f1739e7fcf0d978f588a5f4c9d6b77f57aa64a7
DanielM3218 <daniel.inon.melki@gmail.com>
2025-04-29 15:32:39 +0200

    made everything prettier

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,29 +1,27 @@
-import { useEffect , useState } from "react";
-import { getAllPokemons , Pokemon } from "../api/fetchList";
+import { useEffect, useState } from "react";
+import { getAllPokemons, Pokemon } from "../api/fetchList";
 import "../styles/PokemonList.css";
 
 export default function PokemonList() {
-    const [pokemonList , setPokemonList] = useState<Pokemon[]>([]);
-    const [loading , setLoading] = useState(true);
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        getAllPokemons()
-          .then((data) => {
-            setPokemonList(data);
-            setLoading(false);
-          })
-          .catch((error) => {
-            console.error("Error:", error);
-            setLoading(false);
-          });
-      }, []);
+  useEffect(() => {
+    getAllPokemons()
+      .then((data) => {
+        setPokemonList(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        setLoading(false);
+      });
+  }, []);
 
+  if (loading) return <div>Loading...</div>;
 
-
-    if (loading) return <div>Loading...</div>;
-
-      return (
-        <div className="pokemon-list-container">
+  return (
+    <div className="pokemon-list-container">
       <ul className="pokemon-list">
         {pokemonList.map((pokemon) => (
           <li key={pokemon.pokedexNumber} className="pokemon-item">
@@ -32,7 +30,5 @@ export default function PokemonList() {
         ))}
       </ul>
     </div>
-      );
-
-
-    }
+  );
+}
