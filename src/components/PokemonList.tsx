80156b56639c4a15e1557fff4c631515c92fb1c1
DanielM3218 <daniel.inon.melki@gmail.com>
2025-04-29 13:18:50 +0200

    created a fetch call and a pokemon list component

---
+++ src/components/PokemonList.tsx
@@ -0,0 +1,37 @@
+import { useEffect , useState } from "react";
+import { getAllPokemons , Pokemon } from "../api/fetchList";
+
+export default function PokemonList() {
+    const [pokemonList , setPokemonList] = useState<Pokemon[]>([]);
+    const [loading , setLoading] = useState(true);
+
+    useEffect(() => {
+        getAllPokemons()
+          .then((data) => {
+            setPokemonList(data);
+            setLoading(false);
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+            setLoading(false);
+          });
+      }, []);
+
+
+
+    if (loading) return <div>Loading...</div>;
+
+      return (
+        <div className="pokemon-container">
+      <ul className="pokemon-list">
+        {pokemonList.map((pokemon) => (
+          <li key={pokemon.pokedexNumber} className="pokemon-item">
+            #{pokemon.pokedexNumber} - {pokemon.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+      );
+
+
+    }
